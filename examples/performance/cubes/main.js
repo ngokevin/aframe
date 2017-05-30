9b5c7159e4646717d169e48bceeb1e04ea6f1a03
Kevin Ngo <me@ngokevin.com>
2017-05-30 15:33:37 -0700

    add raycasters to cube perf example

--- examples/performance/cubes/main.js
+++ examples/performance/cubes/main.js
@@ -1,49 +1,71 @@
 /* global AFRAME */
 AFRAME.registerComponent('main', {
   init: function () {
-    var urlParams = this.getUrlParams();
-    var defaultNumObjects = 5000;
-    var numObjects = urlParams.numobjects || defaultNumObjects;
-    this.cubeDistributionWidth = 100;
-    for (var i = 0; i < numObjects; i++) {
-      var cubeEl = document.createElement('a-entity');
+    this.urlParams = getUrlParams();
+    this.createCubes();
+    this.createRaycaster();
+  },
 
+  createCubes: function () {
+    var cubeEl;
+    var i;
+    var numObjects;
+    var urlParams = this.urlParams;
+
+    numObjects = urlParams.numobjects || 5000;
+
+    for (i = 0; i < numObjects; i++) {
+      cubeEl = document.createElement('a-entity');
       if (urlParams.component) {
         cubeEl.setAttribute(urlParams.component, '');
       }
-      cubeEl.setAttribute('position', this.getRandomPosition());
+      cubeEl.setAttribute('position', getRandomPosition(100));
       cubeEl.setAttribute('geometry', {primitive: 'box'});
-      cubeEl.setAttribute('material', {color: this.getRandomColor(), shader: 'flat'});
+      cubeEl.setAttribute('material', {color: getRandomColor(), shader: 'flat'});
       this.el.sceneEl.appendChild(cubeEl);
     }
   },
 
-  getUrlParams: function () {
-    var match;
-    var pl = /\+/g;  // Regex for replacing addition symbol with a space
-    var search = /([^&=]+)=?([^&]*)/g;
-    var decode = function (s) { return decodeURIComponent(s.replace(pl, ' ')); };
-    var query = window.location.search.substring(1);
-    var urlParams = {};
+  createRaycaster: function () {
+    var camera;
+    var i;
+    var raycasterEl;
+    var urlParams = this.urlParams;
 
-    match = search.exec(query);
-    while (match) {
-      urlParams[decode(match[1])] = decode(match[2]);
-      match = search.exec(query);
+    if (!urlParams.numraycasters) { return; }
+
+    camera = document.querySelector('a-camera');
+    for (i = 0; i < urlParams.numraycasters; i++) {
+      raycasterEl = document.createElement('a-cursor');
+      camera.appendChild(raycasterEl);
     }
-    return urlParams;
-  },
-
-  getRandomPosition: function () {
-    var cubeDistributionWidth = this.cubeDistributionWidth;
-    return {
-      x: Math.random() * cubeDistributionWidth - cubeDistributionWidth / 2,
-      y: Math.random() * cubeDistributionWidth - cubeDistributionWidth / 2,
-      z: Math.random() * cubeDistributionWidth - cubeDistributionWidth
-    };
-  },
-
-  getRandomColor: function () {
-    return '#' + ('000000' + Math.random().toString(16).slice(2, 8).toUpperCase()).slice(-6);
   }
 });
+
+function getRandomColor () {
+  return '#' + ('000000' + Math.random().toString(16).slice(2, 8).toUpperCase()).slice(-6);
+}
+
+function getRandomPosition (distribution) {
+  return {
+    x: Math.random() * distribution - distribution / 2,
+    y: Math.random() * distribution - distribution / 2,
+    z: Math.random() * distribution - distribution
+  };
+}
+
+function getUrlParams () {
+  var decode = function (s) { return decodeURIComponent(s.replace(pl, ' ')); };
+  var match;
+  var pl = /\+/g;  // Regex for replacing addition symbol with a space.
+  var query = window.location.search.substring(1);
+  var search = /([^&=]+)=?([^&]*)/g;
+  var urlParams = {};
+
+  match = search.exec(query);
+  while (match) {
+    urlParams[decode(match[1])] = decode(match[2]);
+    match = search.exec(query);
+  }
+  return urlParams;
+}
