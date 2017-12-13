26b241a713cf754f519d49592c37c513dd8c40d5
Kevin Ngo <me@ngokevin.com>
2017-12-13 04:05:46 -0800

    dist

--- src/index.js
+++ src/index.js
@@ -83,7 +83,7 @@ require('./core/a-mixin');
 require('./extras/components/');
 require('./extras/primitives/');
 
-console.log('A-Frame Version: 0.7.0 (Date 2017-12-07, Commit #21cc891)');
+console.log('A-Frame Version: 0.7.0 (Date 2017-12-13, Commit #bce7663b)');
 console.log('three Version:', pkg.dependencies['three']);
 console.log('WebVR Polyfill Version:', pkg.dependencies['webvr-polyfill']);
 
